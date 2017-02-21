8eddb2b5acbd093da021b5c670fc720e879295d0
Elio Tohme <eliotohme@gmail.com>
2017-02-21 17:32:37 +0200

    added line chart with pie chart in graph view

--- XMS_PM/public/js/graph.js
+++ XMS_PM/public/js/graph.js
@@ -2,7 +2,7 @@
 var chart;
 
 //keps track of previous info for linechart
-var previousinfo; 
+var previouslineinfo; 
 
 //request data from mongo
 function requestData() {
@@ -15,18 +15,45 @@ function requestData() {
         success: function(point) {
             //on success count distinct channel_name
             var channelCount = _.countBy(point, 'channel_name');
-
+            
             //transform object to array
-            multiArray = [];
+            channelCountArray = [];
             for(var key in channelCount) { 
-                multiArray.push([ key, channelCount[key] ]); 
+                channelCountArray.push([ key, channelCount[key] ]); 
+            }
+
+            var watchedbytime = _(point).groupBy('channel_name')
+                                        .map(function(item, itemId) {
+                                            var obj = {};
+                                            obj[itemId] = _.countBy(item, 'timestamp');
+                                            return obj;
+                                        }).valueOf();
+
+            for(var key in watchedbytime) {
+                charttimestampinfo = [];
+                for(var channel in watchedbytime[key]) {
+                    for(var timetags in watchedbytime[key][channel]) { 
+                        charttimestampinfo.push([ timetags, watchedbytime[key][channel][timetags] ]); 
+                    }
+                }
+                //TODO complete if statment to update if new channel is added
+                if(!true) {
+                    chart.addSeries({
+                        name: Object.keys(watchedbytime[key])[0],
+                        data: charttimestampinfo,
+                        type: 'spline',
+                    });
+                } else {
+                    chart.series[key + 1].setData(charttimestampinfo, true);
+                }
+                previouslineinfo = watchedbytime;
             }
 
             // add array to series data
-            chart.series[0].setData(multiArray, true);
-            
+            chart.series[0].setData(channelCountArray, true);
+
             // call it again after one second
-            setTimeout(requestData, 10000);    
+            setTimeout(requestData, 5000);    
         },
     });
 }
@@ -38,12 +65,16 @@ chart = new Highcharts.Chart({
     },
     chart: {
         renderTo: 'container',
+        zoomType: 'x',
         events: {
             load: requestData
         }
     },
     title: {
         text: 'Channel watched'
+    },
+    xAxis: {
+            type: 'datetime'
     },
     plotOptions: {
         pie: {
@@ -68,5 +99,15 @@ chart = new Highcharts.Chart({
         tooltip: {
             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
         },
+        center: [100, 80],
+        size: 100,
+        showInLegend: false,
+        dataLabels: {
+            enabled: false
+        }
+    },{
+        type: 'spline',
+        name: 'Average',
+        data: [[1,3], [2,2.67], [3,3], [4,6.33], [5,3.33]],
     }],
 });
