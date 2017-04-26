10fb7d408d774ac9a461760a66acf5c5fdff4b1b
Elio Tohme <eliotohme@gmail.com>
2017-04-26 14:52:34 +0300

    added favicon

--- XMS_PM/resources/views/layouts/app.blade.php
+++ XMS_PM/resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>XMS People Metering</title>
+    <link rel="shortcut icon" href={{url('images/favicon.png')}}> 
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
