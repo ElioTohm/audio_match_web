bb1f86c38b2b7585e8d7935cba1261220482d48a
elio_tohme <eliotohme@gmail.com>
2017-09-27 16:24:42 +0300

    kept table as origin

--- XMS_PM/public/js/report.js
+++ XMS_PM/public/js/report.js
@@ -18,7 +18,7 @@ $('#submit_time').click(function () {
         {
             console.log(data);
 
-            // $('table').html("");
+            $('tbody').html("");
             for (var i = 0; i < data.length; i++) {
                 for (var j = 0; j < data[i].clients.length; j++) {
                     var time = data[i].clients[j].timestamp * 30 ;
@@ -27,10 +27,9 @@ $('#submit_time').click(function () {
                     var seconds = time - minutes * 60 - hours * 3600;
                     
                     var element = "<tr><td>"+data[i]._id.channel_name+"</td><td>"+data[i].clients[j].client_id+"</td><td>"+hours+"</td><td>"+minutes+"</td><td>"+seconds+"</td></tr>";
+                    $('tbody').append(element);
                 }
-                $('table').append("<tbody>" + element + "</tbody>");
             }
-            
         },
         error:function (error)
         {
